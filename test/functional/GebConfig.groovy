13831632c12f8d44828f5b647287e522c088ca26
Mike Calvo <mike@citronellasoftware.com>
2014-12-08 20:57:52 -0600

    Fixed error with import on htmlunit

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,9 +1,8 @@
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 
 //driver = {
-//  def driver = new HtmlUnitDriver()
+//  def driver = new org.openqa.selenium.htmlunit.HtmlUnitDriver()
 //  driver.javascriptEnabled = true
 //  driver
 //}
