e2f29cd0c95b170ee3723150766a90765095a36c
Mike Calvo <mike@citronellasoftware.com>
2014-12-08 22:28:07 -0600

    More cleanup of geb stuff

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,13 +1,6 @@
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 
-//driver = {
-//  def driver = new org.openqa.selenium.htmlunit.HtmlUnitDriver()
-//  driver.javascriptEnabled = true
-//  driver
-//}
-
-
 driver = {
   new PhantomJSDriver(new DesiredCapabilities())
 }
