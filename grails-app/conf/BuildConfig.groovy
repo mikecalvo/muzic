bc8c7286bbef70796f4dfba24ebac1c06528503e
Mike Calvo <mike@citronellasoftware.com>
2015-03-21 00:09:00 -0500

    functional tests improvements

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,9 +12,9 @@ grails.project.fork = [
     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
     // configure settings for the test-app JVM, uses the daemon by default
-    test   :  [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    test   :  false, //[maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
     // configure settings for the run-app JVM
-    run    :  [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
+    run    :  false, // [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
     // ******* IMPORTANT **********
     // To enable debugging in intellij, set run: false
     // Functional tests require it to not be false
