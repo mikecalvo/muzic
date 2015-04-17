3e0b4bd2585694c13a2d497a0d122da0ff42bdec
Mike Calvo <mike.calvo@target.com>
2015-04-17 08:44:44 -0500

    Added chrome driver node dependency.  Added windows-specific npm and grunt config stuff
    # Conflicts:
    #	test/functional/GebConfig.groovy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,37 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks("grunt-shell");
 
-  grunt.initConfig({
-    shell: {
-      options: {
-        stdout: true
-      },
-      localInstall: {
-        command: "./node_modules/.bin/bower update --force --quiet --offline"
-      },
-
-      webInstall: {
-        command: "./node_modules/.bin/bower update --force --quiet"
+  if (process.platform == 'win32') {
+    grunt.initConfig({
+      shell: {
+        options: {
+          stdout: true
+        },
+        localInstall: {
+          command: "node_modules\\.bin\\bower update --quiet --offline --force"
+        },
+        webInstall: {
+          command: "node_modules\\.bin\\bower update --quiet --force"
+        }
       }
-    }
-  });
+    });
+  }
+
+  else {
+    grunt.initConfig({
+      shell: {
+        options: {
+          stdout: true
+        },
+        localInstall: {
+          command: "node_modules/.bin/bower update --quiet --offline --force"
+        },
+        webInstall: {
+          command: "node_modules/.bin/bower update --quiet --force"
+        }
+      }
+    });
+  }
 
   grunt.registerTask("localInstall", [ "shell:localInstall"]);
   grunt.registerTask("webInstall", [ "shell:webInstall"]);
