fd6148481666a4d6b5cd0549dadb2c21cc9f4868
Mike Calvo <mike.calvo@target.com>
2015-04-17 08:44:44 -0500

    Added chrome driver node dependency.  Added windows-specific npm and grunt config stuff
    # Conflicts:
    #	test/functional/GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,8 +1,9 @@
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.phantomjs.PhantomJSDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 
 driver = {
-  new PhantomJSDriver(new DesiredCapabilities())
-  // new ChromeDriver(new DesiredCapabilities())
+  def path = System.properties['os.name'].toLowerCase().contains('windows') ?
+      'node_modules\\.bin\\chromedriver.exe' : './node_modules/.bin/chromedriver'
+  System.setProperty("webdriver.chrome.driver", path);
+  new ChromeDriver(new DesiredCapabilities())
 }
